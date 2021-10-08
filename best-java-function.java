61f774573a6cdf8c2ac4e1bff36ea502f49a09cf
Its_Power ▼ <34723487+ItsPower@users.noreply.github.com>
2021-10-08 10:11:36 +0200

    Create best-java-function.java

---
+++ best-java-function.java
@@ -0,0 +1,7 @@
+public class BestFunction {
+ 
+   protected final @SafeVarargs synchronized static strictfp boolean $validJava(boolean φ, Class<?>[]... wheee) throws Throwable {
+        do do do do break; while (false); while (false); while (false); while (false);
+        return φ |= φ &= φ |= !false;
+     
+}
